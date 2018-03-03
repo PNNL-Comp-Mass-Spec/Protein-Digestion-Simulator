c4276fd700afaf55711377805101f5475b86f911
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-02 22:30:30 -0800

    Use auto properties, and use a dictionary instead of a Hashtable

--- clsCalculatepI.vb
+++ clsCalculatepI.vb
@@ -6,12 +6,11 @@ Option Strict On
 '
 ' Written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA)
 ' Copyright 2005, Battelle Memorial Institute.  All Rights Reserved.
-'
-' Last Modified August 26, 2005
 
 Public Class clspICalculation
 
     Public Sub New()
+        mAminoAcids = New Dictionary(Of Char, AA)
         InitializeLocalVariables()
     End Sub
 
@@ -62,43 +61,30 @@ Public Class clspICalculation
 #End Region
 
 #Region "Classwide Variables"
-    Private mAminoAcids() As AA
-    Private mAminoAcidLookup As Hashtable            ' Allows for fast lookup of the index of a given amino acid in mAminoAcids()
-
-    Private mHydrophobicityType As eHydrophobicityTypeConstants
-    Private mReportMaximumpI As Boolean
-    Private mSequenceWidthToExamineForMaximumpI As Integer
-
+    Private ReadOnly mAminoAcids As Dictionary(Of Char, AA)
 #End Region
 
 #Region "Processing Options Interface Functions"
-    Public Property HydrophobicityType() As eHydrophobicityTypeConstants
-        Get
-            Return mHydrophobicityType
-        End Get
-        Set(Value As eHydrophobicityTypeConstants)
-            mHydrophobicityType = Value
-        End Set
-    End Property
 
-    Public Property ReportMaximumpI() As Boolean
-        Get
-            Return mReportMaximumpI
-        End Get
-        Set(Value As Boolean)
-            mReportMaximumpI = Value
-        End Set
-    End Property
+    ''' <summary>
+    ''' Hydrophobicity type
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property HydrophobicityType As eHydrophobicityTypeConstants
 
-    Public Property SequenceWidthToExamineForMaximumpI() As Integer
-        Get
-            Return mSequenceWidthToExamineForMaximumpI
-        End Get
-        Set(Value As Integer)
-            If Value < 1 Then mSequenceWidthToExamineForMaximumpI = 1
-            mSequenceWidthToExamineForMaximumpI = Value
-        End Set
-    End Property
+    ''' <summary>
+    ''' When true, examine the protein residues in chunks of SequenceWidthToExamineForMaximumpI,
+    ''' compute the pI for each chunk, then report the largest pI
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property ReportMaximumpI As Boolean
+
+    ''' <summary>
+    ''' Number of residues to use for computation of pI when ReportMaximumpI is true
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property SequenceWidthToExamineForMaximumpI As Integer
+
 #End Region
 
     Private Function CalculateCharge(pH As Double, numC As Integer, numD As Integer, numE As Integer, numH As Integer, numK As Integer, numR As Integer, numY As Integer) As Double
@@ -120,46 +106,44 @@ Public Class clspICalculation
     End Function
 
     Private Function CalculateHydrophobicity(seq As String, HT As eHydrophobicityTypeConstants) As Double
-        Dim objMatch As Object
-        Dim li As Integer, i As Integer
-        Dim Sum As Double, Num As Integer
-        Dim strResidue As String
+        Dim runningSum As Double = 0
+        Dim residueCount = 0
 
-        Sum = 0
-        Num = 0
+        Dim aaInfo = New AA()
 
         For li = 1 To seq.Length
-            strResidue = Char.ToUpper(seq.Chars(li - 1))
+            Dim residue = Char.ToUpper(seq.Chars(li - 1))
 
             Try
-                objMatch = mAminoAcidLookup.Item(strResidue)
-                If Not objMatch Is Nothing Then
-                    i = CInt(objMatch)
-
-                    Select Case HT
-                        Case eHydrophobicityTypeConstants.HW
-                            Sum += mAminoAcids(i).HW
-                        Case eHydrophobicityTypeConstants.KD
-                            Sum += mAminoAcids(i).KD
-                        Case eHydrophobicityTypeConstants.Eisenberg
-                            Sum += mAminoAcids(i).Eisenberg
-                        Case eHydrophobicityTypeConstants.GES
-                            Sum += mAminoAcids(i).GES
-                        Case eHydrophobicityTypeConstants.MeekPH7p4
-                            Sum += mAminoAcids(i).MeekPH7p4
-                        Case eHydrophobicityTypeConstants.MeekPH2p1
-                            Sum += mAminoAcids(i).MeekPH2p1
-                    End Select
-                    Num += 1
-
+                If Not mAminoAcids.TryGetValue(residue, aaInfo) Then
+                    Continue For
                 End If
+
+                Select Case HT
+                    Case eHydrophobicityTypeConstants.HW
+                        runningSum += aaInfo.HW
+                    Case eHydrophobicityTypeConstants.KD
+                        runningSum += aaInfo.KD
+                    Case eHydrophobicityTypeConstants.Eisenberg
+                        runningSum += aaInfo.Eisenberg
+                    Case eHydrophobicityTypeConstants.GES
+                        runningSum += aaInfo.GES
+                    Case eHydrophobicityTypeConstants.MeekPH7p4
+                        runningSum += aaInfo.MeekPH7p4
+                    Case eHydrophobicityTypeConstants.MeekPH2p1
+                        runningSum += aaInfo.MeekPH2p1
+                End Select
+
+                residueCount += 1
+
             Catch ex As Exception
                 ' Residue is not present so ignore it
             End Try
 
         Next li
-        If Num > 0 Then
-            Return Sum / Num
+
+        If residueCount > 0 Then
+            Return runningSum / residueCount
         Else
             Return 0
         End If
@@ -300,9 +284,11 @@ Public Class clspICalculation
 
     End Function
 
-    Private Sub DefineAminoAcid(ByRef udtAminoAcids() As AA, ByRef htAminoAcidLookup As Hashtable, intIndex As Integer, str1LetterSymbol As String, dblHW As Double, dblKD As Double, dblEisenberg As Double, dblGES As Double, dblMeekPH7p4 As Double, dblMeekPH2p1 As Double)
+    Private Sub AddAminoAcid(str1LetterSymbol As Char, dblHW As Double, dblKD As Double, dblEisenberg As Double, dblGES As Double, dblMeekPH7p4 As Double, dblMeekPH2p1 As Double)
 
-        With udtAminoAcids(intIndex)
+        Dim aaInfo = New AA()
+
+        With aaInfo
             .Aname = str1LetterSymbol
             .HW = dblHW
             .KD = dblKD
@@ -312,15 +298,15 @@ Public Class clspICalculation
             .MeekPH2p1 = dblMeekPH2p1
         End With
 
-        htAminoAcidLookup.Add(str1LetterSymbol, intIndex)
+        mAminoAcids.Add(str1LetterSymbol, aaInfo)
 
     End Sub
 
     Private Sub InitializeLocalVariables()
 
-        mHydrophobicityType = eHydrophobicityTypeConstants.HW
-        mReportMaximumpI = False
-        mSequenceWidthToExamineForMaximumpI = 10
+        HydrophobicityType = eHydrophobicityTypeConstants.HW
+        ReportMaximumpI = False
+        SequenceWidthToExamineForMaximumpI = 10
 
         LoadAminoAcids()
 
@@ -328,35 +314,27 @@ Public Class clspICalculation
 
     Private Sub LoadAminoAcids()
 
-        Const AminoAcidCount As Integer = 19
+        mAminoAcids.Clear()
 
-        If mAminoAcidLookup Is Nothing Then
-            mAminoAcidLookup = New Hashtable
-        Else
-            mAminoAcidLookup.Clear()
-        End If
-
-        ReDim mAminoAcids(AminoAcidCount - 1)
-
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 0, "A", -0.5, 1.8, 0.25, -1.6, 0.5, -0.1)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 1, "C", -1, 2.5, 0.04, -2, -6.8, -2.2)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 2, "D", 3, -3.5, -0.72, 9.2, -8.2, -2.8)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 3, "E", 3, -3.5, -0.62, 8.2, -16.9, -7.5)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 4, "F", -2.5, 2.8, 0.61, -3.7, 13.2, 13.9)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 5, "G", 0, -0.4, 0.16, -1, 0, -0.5)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 6, "H", -0.5, -3.2, -0.4, 3, -3.5, 0.8)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 7, "I", -1.8, 4.5, 0.73, -3.1, 13.9, 11.8)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 8, "K", 3, -3.9, -1.1, 8.8, 0.1, -3.2)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 9, "L", -1.8, 3.8, 0.53, -2.8, 8.8, 10)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 10, "M", -1.3, 1.9, 0.26, -3.4, 4.8, 7.1)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 11, "N", 0.2, -3.5, -0.64, 4.8, 0.8, -1.6)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 12, "P", 0, -1.6, -0.07, 0.2, 6.1, 8)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 13, "R", 3, -4.5, -1.8, 12.3, 0.8, -4.5)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 14, "S", 0.3, -0.8, -0.26, -0.6, 1.2, -3.7)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 15, "T", -0.4, -0.7, -0.18, -1.2, 2.7, 1.5)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 16, "V", -1.5, 4.2, 0.54, -2.6, 2.1, 3.3)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 17, "W", -3.4, -0.9, 0.37, -1.9, 14.9, 18.1)
-        DefineAminoAcid(mAminoAcids, mAminoAcidLookup, 18, "Y", -2.3, -1.3, 0.02, 0.7, 6.1, 8.2)
+        AddAminoAcid("A"c, -0.5, 1.8, 0.25, -1.6, 0.5, -0.1)
+        AddAminoAcid("C"c, -1, 2.5, 0.04, -2, -6.8, -2.2)
+        AddAminoAcid("D"c, 3, -3.5, -0.72, 9.2, -8.2, -2.8)
+        AddAminoAcid("E"c, 3, -3.5, -0.62, 8.2, -16.9, -7.5)
+        AddAminoAcid("F"c, -2.5, 2.8, 0.61, -3.7, 13.2, 13.9)
+        AddAminoAcid("G"c, 0, -0.4, 0.16, -1, 0, -0.5)
+        AddAminoAcid("H"c, -0.5, -3.2, -0.4, 3, -3.5, 0.8)
+        AddAminoAcid("I"c, -1.8, 4.5, 0.73, -3.1, 13.9, 11.8)
+        AddAminoAcid("K"c, 3, -3.9, -1.1, 8.8, 0.1, -3.2)
+        AddAminoAcid("L"c, -1.8, 3.8, 0.53, -2.8, 8.8, 10)
+        AddAminoAcid("M"c, -1.3, 1.9, 0.26, -3.4, 4.8, 7.1)
+        AddAminoAcid("N"c, 0.2, -3.5, -0.64, 4.8, 0.8, -1.6)
+        AddAminoAcid("P"c, 0, -1.6, -0.07, 0.2, 6.1, 8)
+        AddAminoAcid("R"c, 3, -4.5, -1.8, 12.3, 0.8, -4.5)
+        AddAminoAcid("S"c, 0.3, -0.8, -0.26, -0.6, 1.2, -3.7)
+        AddAminoAcid("T"c, -0.4, -0.7, -0.18, -1.2, 2.7, 1.5)
+        AddAminoAcid("V"c, -1.5, 4.2, 0.54, -2.6, 2.1, 3.3)
+        AddAminoAcid("W"c, -3.4, -0.9, 0.37, -1.9, 14.9, 18.1)
+        AddAminoAcid("Y"c, -2.3, -1.3, 0.02, 0.7, 6.1, 8.2)
 
     End Sub
 
