e9e7625b0e106f098f83de87c587968ed2801533
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-15 15:49:38 -0700

    Add paragraphs to documentation comments

--- ProteinDigestionSimulator/SearchRange.cs
+++ ProteinDigestionSimulator/SearchRange.cs
@@ -16,11 +16,14 @@ namespace ProteinDigestionSimulator
     // Copyright 2018 Battelle Memorial Institute
 
     /// <summary>
+    /// <para>
     /// This class can be used to search a list of values for a given value, plus or minus a given tolerance
     /// The input list need not be sorted, since mPointerIndices[] will be populated when the data is loaded,
     /// after which the data array will be sorted
-    ///
+    /// </para>
+    /// <para>
     /// To prevent this behavior, and save memory by not populating mPointerIndices, set mUsePointerIndexArray = False
+    /// </para>
     /// </summary>
     public class SearchRange
     {
