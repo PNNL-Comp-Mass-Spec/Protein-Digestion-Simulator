8829d29aaa7234776ee4d2a06db88ddcef12ec81
alchemistmatt <matthew.monroe@pnnl.gov>
2026-05-26 18:08:30 -0700

    Return null instead of default

--- ProteinDigestionSimulator/ProteinFileParser.cs
+++ ProteinDigestionSimulator/ProteinFileParser.cs
@@ -44,9 +44,10 @@ namespace ProteinDigestionSimulator
     /// </summary>
     public class ProteinFileParser : ProcessFilesBase
     {
-        // Ignore Spelling: addnl, ComputepI, Cys, Cysteine, fasta, gi, hydrophobicity, Ile, isoelectric, Leu, pre, Sep, SepChar, silico, varchar
+        // Ignore Spelling: addnl, ComputepI, Cys, Cysteine, fasta, gi, hydrophobicity, Ile, isoelectric, Leu,
+        // Ignore Spelling: pre, Sep, SepChar, silico, Subtask, varchar
 
-        private const string PROGRAM_DATE = "November 20, 2021";
+        private const string PROGRAM_DATE = "May 26, 2026";
 
         private const int SCRAMBLING_CACHE_LENGTH = 4000;
         private const string PROTEIN_PREFIX_SCRAMBLED = "Random_";
@@ -546,7 +547,7 @@ namespace ProteinDigestionSimulator
                 return mProteins[index];
             }
 
-            return default;
+            return null;
         }
 
         /// <summary>
